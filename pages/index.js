0093ea46fe5d7f492b2ce1afed791555f10bbd40
Harshini Sriramulu <sriharshini.sriram@gmail.com>
2022-04-01 20:24:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Need Advice on Advice?: An Interview Study on how Experts Write General Security Advice" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
