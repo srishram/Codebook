3ff47e485ab0b9cb3914d7b3db6a49fe0a75de9b
Harshini Sriramulu <sriharshini.sriram@gmail.com>
2022-04-01 20:31:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Advice22.netlify.app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
