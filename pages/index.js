22daa63de9180521ba88190caf93520d406b040c
Harshini Sriramulu <sriharshini.sriram@gmail.com>
2022-10-10 21:24:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
            <ul>
     
           <li>  <a href= "https://drive.google.com/file/d/1J5YVLQUhm4sKo61nv4SEDiRAZdxNo15S/view">Code book </a> </li>
-          <li> <a href ="https://drive.google.com/file/d/1yenbkNAysAm2yteKY1Wd20AwmcwToV0x/view>Interview Guide</a> </li>
+          <li> <a href ="https://drive.google.com/file/d/1yenbkNAysAm2yteKY1Wd20AwmcwToV0x/view">Interview Guide</a> </li>
                
            </ul>
     
